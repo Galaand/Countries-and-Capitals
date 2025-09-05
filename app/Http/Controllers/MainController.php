05c5e5b5af47acbe4fc932b79e3a97a6aa29a179
André Matter Lassakoski <andre.mlassakoski@gmail.com>
2025-09-04 23:19:01 -0300

    006

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\View\View;
 
 class MainController extends Controller
@@ -95,6 +96,48 @@ class MainController extends Controller
             'currentQuestion' => $current_question,
             'answers'         => $answers
         ]);
+    }
+
+    public function answer($answer)   {
+
+        try {
+            $answer = Crypt::decryptString($answer);
+        } catch (\Exception $e) {
+            return redirect()->route('game');
+        }
+
+        //game logic
+
+        $quiz = session('quiz');
+        $current_question = session('current_question') - 1;
+        $correct_answer = $quiz[$current_question]['correct_answer'];
+        $correct_answers = session('correct_answers');
+        $wrong_answers = session('wrong_answers');
+
+        if ($answer === $correct_answer) {
+            $correct_answers++;
+            $quiz[$current_question]['correct'] = true;
+        } else {
+            $wrong_answers++;
+            $quiz[$current_question]['correct'] = false;
+        }
+
+        session()->put([
+            'quiz'            => $quiz,
+            'correct_answers' => $correct_answers,
+            'wrong_answers'   => $wrong_answers
+        ]);
+
+        $data = [
+            'country'        => $quiz[$current_question]['country'],
+            'correct_answer' => $correct_answer,
+            'choice_answer'  => $answer,
+            'currentQuestion' => $current_question,
+            'totalQuestions'  => session('total_questions')
+        ];
+
+        return view('answer')->with($data);
+
     }
 
 }
---
+++ resources/views/components/answer.blade.php
@@ -0,0 +1,4 @@
+<div class="col-6 text-center">
+    <a href="{{ route('answer', Crypt::encryptString($capital)) }}" class="text-decoration-none"></a>
+    <p class="response-option">{{ $capital }}</p>
+</div>
